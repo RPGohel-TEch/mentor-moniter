ab8faf3e122fac5e95254fadcb39349340985d2d
Rahul Gohel <rahulgohel8317@gmail.com>
2022-10-16 17:19:04 +0530

    done setting section

--- mentor-moniter-app/src/App.js
+++ mentor-moniter-app/src/App.js
@@ -1,9 +1,18 @@
+import Signup from './components/Signup';
 import './index.css';
+import {  Routes, Route } from "react-router-dom";
+import Layout from './components/Layout';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-     Hello React I am Mentor Moniter
+
+      <Routes>  
+          <Route path='/' element={<Layout />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/signup" element={<Signup />} />  
+      </Routes>
     </div>
   );
 }
